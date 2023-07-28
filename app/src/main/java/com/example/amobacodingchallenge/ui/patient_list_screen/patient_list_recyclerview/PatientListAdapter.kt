6c19f36bec309d53cad6904c9f42a2696b963d7f
Miguel <centenokm.ing@gmail.com>
2023-07-27 22:07:05 -0600

    minor changes

--- app/src/main/java/com/example/amobacodingchallenge/ui/patient_list_screen/patient_list_recyclerview/PatientListAdapter.kt
+++ app/src/main/java/com/example/amobacodingchallenge/ui/patient_list_screen/patient_list_recyclerview/PatientListAdapter.kt
@@ -1,4 +1,52 @@
 package com.example.amobacodingchallenge.ui.patient_list_screen.patient_list_recyclerview
 
-class PatientListAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.example.amobacodingchallenge.R
+import com.example.amobacodingchallenge.databinding.PatientItemCellBinding
+import com.example.amobacodingchallenge.domain.entities.Patient
+
+class PatientListAdapter(
+    private val patientList: MutableList<Patient>,
+    private val onClickAction: ((Int) -> Unit)?
+) : RecyclerView.Adapter<PatientListAdapter.PatientListViewHolder>() {
+
+    inner class PatientListViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+
+        private val binding = PatientItemCellBinding.bind(itemView)
+
+        fun configure(patient: Patient) {
+            binding.nameTextview.text = patient.name
+            binding.actualPatientTextview.text = "Paciente actual"
+
+            Glide.with(view)
+                .load(patient.image.toString())
+                .placeholder(R.drawable.user_icon)
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(binding.patientImageview)
+
+            binding.patientItemCellContainer.setOnClickListener {
+                onClickAction?.invoke(patient.id)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PatientListViewHolder {
+
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.patient_item_cell, parent, false)
+        return PatientListViewHolder(view)
+    }
+
+    override fun getItemCount() = patientList.count()
+
+    override fun onBindViewHolder(holder: PatientListViewHolder, position: Int) {
+        holder.configure(patientList[position])
+    }
 }
