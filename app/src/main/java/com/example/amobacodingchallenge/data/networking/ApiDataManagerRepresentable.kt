860072f07c8c969d556e5a167ee51495ac670545
Miguel <centenokm.ing@gmail.com>
2023-07-28 10:57:41 -0600

    Minor Change

--- app/src/main/java/com/example/amobacodingchallenge/data/networking/ApiDataManagerRepresentable.kt
+++ app/src/main/java/com/example/amobacodingchallenge/data/networking/ApiDataManagerRepresentable.kt
@@ -1,11 +1,13 @@
 package com.example.amobacodingchallenge.data.networking
 
+import com.example.amobacodingchallenge.data.networking.firestore.dto.Document
 import com.example.amobacodingchallenge.data.networking.firestore.dto.PatientDTO
+import com.example.amobacodingchallenge.data.networking.firestore.dto.PatientDetailsDTO
 import com.example.amobacodingchallenge.domain.entities.Patient
 
 interface ApiDataManagerRepresentable {
 
-    suspend fun fetchAllPatients(): List<PatientDTO>
+    suspend fun fetchAllPatients(): List<Document<PatientDTO>>
 
-    suspend fun fetchPatientById(patientId: String): PatientDTO
+    suspend fun fetchPatientById(patientId: String): Document<PatientDetailsDTO>
 }
--- app/src/main/java/com/example/amobacodingchallenge/data/networking/firestore/FirestoreDataManager.kt
+++ app/src/main/java/com/example/amobacodingchallenge/data/networking/firestore/FirestoreDataManager.kt
@@ -1,16 +1,18 @@
 package com.example.amobacodingchallenge.data.networking.firestore
 
 import com.example.amobacodingchallenge.data.networking.ApiDataManagerRepresentable
+import com.example.amobacodingchallenge.data.networking.firestore.dto.Document
 import com.example.amobacodingchallenge.data.networking.firestore.dto.PatientDTO
+import com.example.amobacodingchallenge.data.networking.firestore.dto.PatientDetailsDTO
 import javax.inject.Inject
 
 class FirestoreDataManager @Inject constructor() : ApiDataManagerRepresentable {
 
-    override suspend fun fetchAllPatients(): List<PatientDTO> {
+    override suspend fun fetchAllPatients(): List<Document<PatientDTO>> {
         TODO("Not yet implemented")
     }
 
-    override suspend fun fetchPatientById(patientId: String): PatientDTO {
+    override suspend fun fetchPatientById(patientId: String): Document<PatientDetailsDTO> {
         TODO("Not yet implemented")
     }
 
