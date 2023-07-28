837e2fb19c52b96a6d9c572e9216863ff653c0bb
Miguel <centenokm.ing@gmail.com>
2023-07-27 21:56:29 -0600

    Fix refer error

--- app/src/main/java/com/example/amobacodingchallenge/data/authentication/FirebaseAutenticationService.kt
+++
@@ -1,4 +0,0 @@
-package com.example.amobacodingchallenge.data.authentication
-
-class FirebaseAutenticationService {
-}
--- app/src/main/java/com/example/amobacodingchallenge/domain/use_cases/LoginUseCases.kt
+++ app/src/main/java/com/example/amobacodingchallenge/domain/use_cases/LoginUseCases.kt
@@ -3,7 +3,7 @@ package com.example.amobacodingchallenge.domain.use_cases
 import javax.inject.Inject
 
 class LoginUseCases @Inject constructor(
-    private val service: FirebaseAutenticationServiceRepresentable
+
 ) {
 
 
